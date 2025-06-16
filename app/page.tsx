ee276aa2f9d1b024739219df5dc68b1cf0337d1c
Akshay <akshay.allen26200@gmail.com>
2025-06-16 12:15:42 +0530

    Added globe animations to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { HeroSection } from "@/components/home/HeroSection"
+import { HeroSection2 } from "@/components/home/HeroSection2"
 import { FeaturesSection } from "@/components/home/FeaturesSection"
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center w-full overflow-hidden">
       <div className="w-full max-w-[2000px]">
-        <HeroSection />
+        <HeroSection2 />
         <FeaturesSection />
       </div>
     </div>
