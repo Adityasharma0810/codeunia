90239351cdf53118863553d52f559cec64570fc0
Akshay <akshay.allen26200@gmail.com>
2025-06-17 11:21:26 +0530

    Implemented Forgot Password function plus updated team section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 import { HeroSection2 } from "@/components/home/HeroSection2"
 import { FeaturesSection } from "@/components/home/FeaturesSection"
-export default function HomePage() {
+
+export default function Home() {
   return (
-    <div className="flex flex-col items-center w-full overflow-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="w-full max-w-[2000px]">
         <HeroSection2 />
         <FeaturesSection />
       </div>
-    </div>
+    </main>
   )
 }
