fc266fdcf13a1f58eb687bfd02e3bf298480d173
Arbazx01 <mdarbaz9675@gmail.com>
2025-06-18 14:03:19 +0530

    fixbtn

--- components/home/FeaturesSection.tsx
+++ components/home/FeaturesSection.tsx
@@ -107,7 +107,7 @@ export function FeaturesSection() {
               <span className="absolute inset-0 overflow-hidden rounded-full">
                 <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.8)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
               </span>
-              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950/90 py-2 px-5 ring-1 ring-white/20">
+              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950/90 py-0.5 px-4 ring-1 ring-white/20">
                 <span className="text-white/90">Platform Features</span>
                 <Star className="w-3 h-3 text-emerald-400" />
               </div>
